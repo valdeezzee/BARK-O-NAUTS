b20752cd8c73d9d99d28269d3e3e11ee0901b6c8
valdeezzee <jvalderrama91@hotmail.com>
2015-10-03 21:28:13 -0400

    please commit

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Menu : MonoBehaviour {
+
+
+
+	public void StartGame(){
+
+		Application.LoadLevel("Fetch_The_Ship");
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+}
