1da81e7a4ecfb97f3474360b4f3aab6cdbea4578
valdeezzee <jvalderrama91@hotmail.com>
2015-10-04 19:10:00 -0400

    Added character selection
    Added character selection only to the main menu and not the first level.

--- Assets/Scripts/Main_Menu.cs
+++ Assets/Scripts/Main_Menu.cs
@@ -3,14 +3,72 @@ using System.Collections;
 
 public class Main_Menu : MonoBehaviour {
 
+	public GameObject startButton;
+	public GameObject quitButton;
+	public GameObject beginButton;
+	public GameObject marcus;
+	public GameObject ayame;
+	public GameObject mSpotlight;
+	public GameObject aSpotlight;
+	private Transform marcusORotation;
+	private Transform ayameORotation;
+	private bool changePlayer;
 
+	public float turnSpeed = 20f;
+
+	void Start(){
+		marcusORotation = marcus.transform;
+		ayameORotation = ayame.transform;
+		changePlayer = true;
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+		{
+			if(changePlayer)
+				changePlayer = false;
+			else
+				changePlayer = true;
+		}
+
+		if(changePlayer){
+			rotateMarcus();
+			ayame.transform.rotation = new Quaternion( 0, ayameORotation.rotation.y, 0, 0);
+		}
+		else
+		{
+			rotateAyame();
+			marcus.transform.rotation = new Quaternion( 0, marcusORotation.rotation.y, 0, 0);
+		}
+
+
+	}
 
 	public void StartGame(){
-
-		Application.LoadLevel("Fetch_The_Ship");
+		startButton.SetActive(false);
+		quitButton.SetActive(false);
+		beginButton.SetActive(true);
+		marcus.SetActive(true);
+		ayame.SetActive(true);
+		mSpotlight.SetActive(true);
+		aSpotlight.SetActive(true);
 	}
 
 	public void Quit(){
 		Application.Quit();
 	}
+
+	public void Begin(){
+		Application.LoadLevel("Fetch_The_Ship");
+	}
+
+	private void rotateMarcus(){
+		marcus.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+	}
+
+	private void rotateAyame(){
+		ayame.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+	}
+
+
 }
